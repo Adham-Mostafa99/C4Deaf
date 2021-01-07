1d780b5901bc06d06fca2987e6e451e5da7cb7d4
Aya Hassan <70991772+AyaHassan33@users.noreply.github.com>
2021-01-07 22:54:18 +0200

    add chat_page_activity

---
+++ app/src/main/java/com/example/graduationproject/ui/ChatPageActivity.java
@@ -0,0 +1,16 @@
+package com.example.graduationproject.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.graduationproject.R;
+
+public class ChatPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_page);
+    }
+}
