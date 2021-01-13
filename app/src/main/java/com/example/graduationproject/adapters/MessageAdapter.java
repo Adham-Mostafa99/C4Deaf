13bb300ae1d8d9827c696c233ebf9326bca37aa3
EngAdham <adham.dawey@gmail.com>
2021-01-13 12:07:04 +0200

    add video view and edit normal chat

--- app/src/main/java/com/example/graduationproject/adapters/MessageAdapter.java
+++ app/src/main/java/com/example/graduationproject/adapters/MessageAdapter.java
@@ -15,33 +15,35 @@ import com.example.graduationproject.models.Chat;
 import com.example.graduationproject.models.User;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHolder> {
 
+    //specify type of view
     public static final int MSG_TYPE_LEFT = 0;
-    public static final int MSG_TYPE_RIGHT =1;
+    public static final int MSG_TYPE_RIGHT = 1;
 
 
     private ArrayList<Chat> chats;
     private Context context;
-    private String imageurl;
     //MessageAdapter.OnItemClickListener onItemClickListener;
 
-    public MessageAdapter(ArrayList<Chat> chats, Context context, String imageurl) {
+    public MessageAdapter(ArrayList<Chat> chats, Context context) {
         this.chats = chats;
         this.context = context;
-        this.imageurl = imageurl;
     }
 
     @NonNull
     @Override
     public MessageAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        if(viewType == MSG_TYPE_RIGHT){
+        //do specify message which each user
+        //which may be right for person or left for parentUser
+        if (viewType == MSG_TYPE_RIGHT) {
             View view = LayoutInflater.from(context).inflate(R.layout.chat_item_right, parent, false);
             return new ViewHolder(view);
-        }else{
+        } else {
             View view = LayoutInflater.from(context).inflate(R.layout.chat_item_left, parent, false);
             return new ViewHolder(view);
         }
@@ -49,6 +51,9 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Chat msg = chats.get(position);
+        holder.showMessage.setText(msg.getMessage());
+        holder.timeMessage.setText(msg.getTime());
 
     }
 
@@ -58,19 +63,25 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.ViewHold
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
-        CircleImageView profileImage;
         TextView showMessage;
-
+        TextView timeMessage;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            profileImage = itemView.findViewById(R.id.profile_image);
             showMessage = itemView.findViewById(R.id.show_message);
-
+            timeMessage = itemView.findViewById(R.id.time_message);
             //this.onItemClickListener = onItemClickListener;
 
         }
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        //only for test view type
+        if (position % 2 == 0)
+            return MSG_TYPE_LEFT;
+        else
+            return MSG_TYPE_RIGHT;
+    }
 }
 
--- app/src/main/java/com/example/graduationproject/ui/ChatPageNormal.java
+++ app/src/main/java/com/example/graduationproject/ui/ChatPageNormal.java
@@ -1,16 +1,129 @@
 package com.example.graduationproject.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.graduationproject.R;
+import com.example.graduationproject.adapters.MessageAdapter;
+import com.example.graduationproject.models.Chat;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ChatPageNormal extends AppCompatActivity {
+
+    @BindView(R.id.normal_record)
+    ImageView normalRecord;
+    @BindView(R.id.chat_view)
+    RelativeLayout chatView;
+    VideoView videoView;
+    @BindView(R.id.recycler_view_chat)
+    RecyclerView recyclerViewChat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_page_normal);
+        ButterKnife.bind(this);
+
+        testChat();
+
+        //for test video pop up only
+        normalRecord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPopVideoView();
+                playVideo();
+
+            }
+        });
+
     }
+
+    public void testChat(){
+        ArrayList<Chat> arrayList = new ArrayList<>();
+
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+        arrayList.add(new Chat("per1", "per2", "hello", "10:25 PM"));
+
+        MessageAdapter adapter=new MessageAdapter(arrayList,this);
+        recyclerViewChat.setAdapter(adapter);
+        recyclerViewChat.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+
+    //make popup window or video
+    public void openPopVideoView() {
+        //make the width and height for the pop Window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+        // lets taps outside the popup also dismiss it
+        boolean focusable = true;
+
+        //inflate new layout with specific layout(pop_layout) for the pop window
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popLayout = layoutInflater.inflate(R.layout.vedio_message, null);
+
+        //create instance of popupWindow by specific view, width, and height
+        PopupWindow popupWindow = new PopupWindow(popLayout, width, height, focusable);
+
+        //show the created instance in specific location
+        popupWindow.showAtLocation(chatView, Gravity.CENTER, 0, 0);
+
+        //declare videoView which will appear in pop up window
+        videoView = popLayout.findViewById(R.id.video_view);
+    }
+
+    public void playVideo() {
+        //create the path of video
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.test;
+
+        //create uri with specific path
+        Uri uri = Uri.parse(videoPath);
+
+        //set path to the videoView
+        videoView.setVideoURI(uri);
+
+        //create MediaController for control the video
+        //like play ,stop and etc...
+        MediaController mediaController = new MediaController(this);
+
+        //set this controller to the video view
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
+
+        //start video
+        videoView.start();
+    }
+
 }
